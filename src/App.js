5c482399f2e1682a73cb729141b1cde5cc38be00
Void321 <paulettz@icloud.com>
2022-08-12 17:13:51 +0900

    feat: created search bar and buttons
    -developed search button, categories drop down menu and search bar.
    -styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import "./App.css";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <h1>EZ Cocktails</h1>
-      {/* Header */}
+      <SearchBar />
       {/* Search Bar */}
       {/* Cockail List */}
       {/* Cocktail Details Preview*/}
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,34 @@
 import React from "react";
+import "./SearchBar.css";
 
-export const SearchBar = () => {
+export default function SearchBar() {
   return (
     <section>
       <form>
-        <input type="text" placeholder="Search.." />
+        <input
+          type="text"
+          placeholder="Search for a cocktail..."
+          id="search-bar"
+        />
 
-        <input type="button" value="search" />
+        <input type="button" value="Search" className="btn" />
+
+        <div class="dropdown">
+          <button class="btn">Categories</button>
+          <div class="dropdown-content">
+            {" "}
+            <a href="#" rel="noreferrer" target="_blank">
+              Alcoholic
+            </a>
+            <a href="#" rel="noreferrer" target="_blank">
+              Non-Alcholic
+            </a>
+          </div>
+        </div>
       </form>
       <div className="hint">
         suggested cocktails: cosmo, moscow mule, sazerac...
       </div>
     </section>
   );
-};
+}
