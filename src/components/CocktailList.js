86acc66041934c127d25923241891a4fc5e69278
hmshp <hyemisonne@gmail.com>
2022-08-12 23:59:33 +0900

    Fix typo in component name

---
+++ src/components/CocktailList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components/macro";
+
+
+const CocktailList = () => {
+  // const { cocktails } = useCocktialContext();
+
+  return (
+    <div>Cocktail list</div>
+  );
+};
+
+export default CocktailList;
