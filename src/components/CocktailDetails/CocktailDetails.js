88a352c1e3814e457d53f3601fc6634cbb5fa8d3
viteshbava <vitesh.bava@gmail.com>
2022-08-22 20:40:44 +1200

    Add id logic in url and convert updateSelectedCocktail to use useCallback in case used within a useEffect and added as dependency

--- src/components/CocktailDetails/CocktailDetails.js
+++ src/components/CocktailDetails/CocktailDetails.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 const CocktailDetails = () => {
-  const { id } = useParams();
+  const id = +useParams().id;
+
   return (
     <>
       <h1>Cocktail Details Page</h1>
-      <p>{`Id: ${id}`}</p>
+      <h2>{`Id: ${id}`}</h2>
     </>
   );
 };
