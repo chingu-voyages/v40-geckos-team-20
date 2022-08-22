79af0d33b4c516a0862b8d20faaeae62f2b31afd
viteshbava <vitesh.bava@gmail.com>
2022-08-22 20:57:15 +1200

    Add clicking cocktail logic to cocktail list page to view cocktail details page

--- src/components/CocktailList/CocktailList.js
+++ src/components/CocktailList/CocktailList.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useCocktailListContext } from '../../context/use-context';
 import { Wrapper, Cocktail, CocktailImage } from './CocktailList.styled';
 
@@ -12,12 +13,14 @@ const CocktailList = () => {
 
   const randomCocktails = cocktails?.drinks?.map((cocktail, i) => {
     return (
-      <Cocktail key={i}>
-        <CocktailImage
-          src={`${cocktail.strDrinkThumb}/preview`}
-          alt={cocktail.strDrink}
-        />
-      </Cocktail>
+      <Link key={i} to={`/cocktails/${cocktail.idDrink}`}>
+        <Cocktail>
+          <CocktailImage
+            src={`${cocktail.strDrinkThumb}/preview`}
+            alt={cocktail.strDrink}
+          />
+        </Cocktail>
+      </Link>
     );
   });
 
