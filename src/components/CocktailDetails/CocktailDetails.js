2981bc578bbc467c0ad153a48f3f3669c81c3f73
hmshp <hyemisonne@gmail.com>
2022-08-24 17:25:53 +0900

    Add markup for CocktailDetails.js

--- src/components/CocktailDetails/CocktailDetails.js
+++ src/components/CocktailDetails/CocktailDetails.js
@@ -1,5 +1,20 @@
-import React from 'react';
-import { useParams, Link } from 'react-router-dom';
+import React from "react";
+import { useParams, Link } from "react-router-dom";
+import {
+  Wrapper,
+  Header,
+  CocktailImage,
+  CocktailName,
+  Main,
+  IngredientsWrapper,
+  IngredientsTitle,
+  IngredientsContent,
+  IngredientsItem,
+  RecipeWrapper,
+  RecipeTitle,
+  RecipeContent,
+  RecipeItem,
+} from "./CocktailDetails.styled";
 
 const CocktailDetails = () => {
   const id = +useParams().id;
@@ -7,11 +22,42 @@ const CocktailDetails = () => {
   // use id to update selected cocktail
 
   return (
-    <>
-      <h1>Cocktail Details Page</h1>
-      <h2>{`Id: ${id}`}</h2>
-      <Link to='/'>Back</Link>
-    </>
+    <Wrapper>
+      <Header>
+        <CocktailImage />
+        <CocktailName>Sazerac</CocktailName>
+      </Header>
+      <Main>
+        <IngredientsWrapper>
+          <IngredientsTitle>Ingredients</IngredientsTitle>
+          <IngredientsContent>
+            <IngredientsItem>2 ounces of rye bourbon</IngredientsItem>
+            <IngredientsItem>1/4 ounce simple syrup</IngredientsItem>
+            <IngredientsItem>2 dashes angusta bitters</IngredientsItem>
+            <IngredientsItem>Orange peel for garnish</IngredientsItem>
+          </IngredientsContent>
+        </IngredientsWrapper>
+        <RecipeWrapper>
+          <RecipeTitle>Recipe</RecipeTitle>
+          <RecipeContent>
+            <RecipeItem>
+            Rinse a chilled rocks glass with absinthe, discarding any excess, and set aside.
+            </RecipeItem>
+            <RecipeItem>
+              In a mixing glass, muddle the sugar cube, water and the Peychaud’s and Angostura bitters.
+            </RecipeItem>
+            <RecipeItem>
+              Add the rye and cognac, fill the mixing glass with ice and stir until well-chilled.
+            </RecipeItem>
+            <RecipeItem>
+              Strain into the prepared glass.            </RecipeItem>
+            <RecipeItem>
+              Twist the lemon peel over the drink’s surface to express the peel’s oils, then garnish with the peel.            </RecipeItem>
+          </RecipeContent>
+        </RecipeWrapper>
+      </Main>
+      <Link to="/">Back</Link>
+    </Wrapper>
   );
 };
 
---
+++ src/components/CocktailDetails/CocktailDetails.styled.js
@@ -0,0 +1,53 @@
+import styled from 'styled-components/macro';
+
+export const Wrapper = styled.article`
+  
+`
+
+export const Header = styled.header`
+
+`
+
+export const CocktailImage = styled.img`
+
+`
+
+export const CocktailName = styled.h2`
+
+`
+
+export const Main = styled.section`
+
+`
+
+export const IngredientsWrapper = styled.section`
+
+`
+
+export const IngredientsTitle = styled.h3`
+
+`
+
+export const IngredientsContent = styled.ul`
+  
+`
+
+export const IngredientsItem = styled.li`
+  
+`
+
+export const RecipeWrapper = styled.section`
+
+`
+
+export const RecipeTitle = styled.h3`
+
+`
+
+export const RecipeContent = styled.ol`
+
+`
+
+export const RecipeItem = styled.li`
+
+`
