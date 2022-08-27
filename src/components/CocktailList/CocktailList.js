5e69b5ca27f9eaa77fc332cb244fa08832334ab1
viteshbava <vitesh.bava@gmail.com>
2022-08-27 20:20:25 +1200

    Add Spinner to CocktailList and render conditionally

--- src/components/CocktailList/CocktailList.js
+++ src/components/CocktailList/CocktailList.js
@@ -3,19 +3,23 @@ import { Link } from 'react-router-dom';
 import { useCocktailListContext } from '../../context/use-context';
 import { Wrapper, Cocktail, CocktailImage } from './CocktailList.styled';
 import { CONTEXT_STATUS } from '../../context/constants';
+import Spinner from '../UI/Spinner/Spinner';
 
 const CocktailList = () => {
   const { cocktails, getRandomCocktails } = useCocktailListContext();
   const firstRender = useRef(true);
 
+  const { status, error, drinks } = cocktails;
+  const { IDLE, LOADING, SUCCESS, ERROR } = CONTEXT_STATUS;
+
   useEffect(() => {
-    if (firstRender.current && cocktails.status === CONTEXT_STATUS.IDLE) {
+    if (firstRender.current && status === IDLE) {
       firstRender.current = false;
       getRandomCocktails(6);
     }
-  }, [getRandomCocktails, cocktails.status]);
+  }, [getRandomCocktails, status, IDLE]);
 
-  const randomCocktails = cocktails?.drinks?.map((cocktail, i) => {
+  const cocktailList = drinks?.map((cocktail, i) => {
     return (
       <Cocktail key={i}>
         <Link to={`/cocktails/${cocktail.idDrink}`}>
@@ -28,7 +32,16 @@ const CocktailList = () => {
     );
   });
 
-  return <Wrapper>{randomCocktails}</Wrapper>;
+  const haveDrinks = cocktailList?.length;
+
+  return (
+    <>
+      {status === LOADING && <Spinner />}
+      {status === SUCCESS && haveDrinks && <Wrapper>{cocktailList}</Wrapper>}
+      {status === SUCCESS && !haveDrinks && <p>{'No drinks!'}</p>}
+      {status === ERROR && <p>{error.message}</p>}
+    </>
+  );
 };
 
 export default CocktailList;
