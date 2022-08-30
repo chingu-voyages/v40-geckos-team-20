fc4c6d5b8fd101b859360b13ab508456c5bcf8f8
hmshp <hyemisonne@gmail.com>
2022-08-30 14:23:42 +0900

    Show only cocktails of page 1

--- src/components/CocktailList/CocktailList.js
+++ src/components/CocktailList/CocktailList.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useCocktailListContext } from '../../context/use-context';
 import { Wrapper, Cocktail, CocktailImage } from './CocktailList.styled';
@@ -8,10 +8,15 @@ import { InfoMessage, ErrorMessage } from '../MessageState/MessageState';
 
 const CocktailList = () => {
   const { cocktails, getRandomCocktails } = useCocktailListContext();
+  const [currentPage, setCurrentPage] = useState(1);
   const firstRender = useRef(true);
 
   const { status, error, drinks } = cocktails;
   const { IDLE, LOADING, SUCCESS, ERROR } = CONTEXT_STATUS;
+
+  const allDrinks = drinks?.slice();
+  const chunkedCocktails = allDrinks && chunkArrayInGroups(allDrinks, 6);
+  const cocktailList = generateCocktailListUI();
 
   useEffect(() => {
     if (firstRender.current && status === IDLE) {
@@ -20,18 +25,33 @@ const CocktailList = () => {
     }
   }, [getRandomCocktails, status, IDLE]);
 
-  const cocktailList = drinks?.map((cocktail, i) => {
-    return (
-      <Cocktail key={i}>
-        <Link to={`/cocktails/${cocktail.idDrink}`}>
-          <CocktailImage
-            src={`${cocktail.strDrinkThumb}/preview`}
-            alt={cocktail.strDrink}
-          />
-        </Link>
-      </Cocktail>
-    );
-  });
+  
+
+  function chunkArrayInGroups(arr, size) {
+    let chunkedCocktails = [];
+    for(let i = 0; i < arr.length; i += size) {
+      chunkedCocktails.push(arr.slice(i, i+size));
+    }
+    return chunkedCocktails;
+  }
+
+  function generateCocktailListUI() {
+    if (chunkedCocktails) {
+      const cocktailList = chunkedCocktails[currentPage - 1].map((cocktail, i) => {
+        return (
+          <Cocktail key={i}>
+            <Link to={`/cocktails/${cocktail.idDrink}`}>
+              <CocktailImage
+                src={`${cocktail.strDrinkThumb}/preview`}
+                alt={cocktail.strDrink}
+              />
+            </Link>
+          </Cocktail>
+        );
+      });
+      return cocktailList;
+    }
+  }
 
   const haveDrinks = cocktailList?.length;
 
