b788fb2c26066d9b0306b91b2df2f1a08a17f286
viteshbava <vitesh.bava@gmail.com>
2022-08-31 21:54:13 +1200

    Add some comments to SearchBar.js

--- src/components/SearchBar/Filters/Filters.js
+++ src/components/SearchBar/Filters/Filters.js
@@ -8,16 +8,19 @@ const Filters = () => {
 
   const { status, error, allFilters, selectedFilters } = filters;
 
+  // Display error in console if there is one; can just show empty drop down boxes if there was an error
   useEffect(() => {
     if (status === CONTEXT_STATUS.ERROR) console.log(error.message);
   }, [status, error]);
 
+  // function used to get filter options for any given filter
   const filterOptions = (filterKey) => {
     if (!allFilters?.hasOwnProperty(filterKey)) return [];
     if (!allFilters[filterKey].length) return [];
     return ['Any', ...allFilters[filterKey]];
   };
 
+  // function used to get selected filter for any given filter
   const selectedFilter = (filterKey) => {
     if (!filterOptions(filterKey).length) return '-';
     if (!selectedFilters?.hasOwnProperty(filterKey)) return 'Any';
@@ -25,6 +28,7 @@ const Filters = () => {
     return selectedFilters[filterKey][0];
   };
 
+  // function used to update the given filter with a given value
   const updateFilter = (filterKey, newVal) =>
     updateFilters({ [filterKey]: newVal === 'Any' ? [] : [newVal] });
 
