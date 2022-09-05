33a842c42f074a50475c28a223040a91d837e7af
viteshbava <vitesh.bava@gmail.com>
2022-09-05 21:56:52 +1200

    Add styled button to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from '../../images/logo.png';
 import { useLocation, Link } from 'react-router-dom';
-import { BackCTAWrapper, HeaderWrapper } from './Header.styled';
+import { HeaderWrapper } from './Header.styled';
 
 export default function Header() {
   const location = useLocation();
@@ -12,13 +12,11 @@ export default function Header() {
   // Note: the reason why the link enclosing the img logo is a direct <a> and not a <Link> is because we want clicking the logo to refresh the app completely (i.e. load new cocktails, and (I think?) clear state) - i.e. as if the user clicks the refresh button in the browser.  Therefore <a> will do this instead of just changing the url with <Link>
 
   return (
-    <HeaderWrapper>
-      <BackCTAWrapper notRoot={notRoot()}>
-        <Link to='/' className='back-cta'>
-          Back
-        </Link>
-      </BackCTAWrapper>
-      <a href='/'>
+    <HeaderWrapper notRoot={notRoot()}>
+      <Link to='/' className='back-cta'>
+        Back
+      </Link>
+      <a className='logo' href='/'>
         <img src={logo} alt='Ez-Logo' />
       </a>
       <h1> EZ Cocktails</h1>
--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -1,44 +1,27 @@
 import styled from 'styled-components/macro';
 import { ButtonStyles } from '../UI/Button.styled';
 
-export const BackCTAWrapper = styled.div`
-  visibility: ${(props) => (props.notRoot ? 'visible' : 'hidden')};
-  position: absolute;
-  left: 0;
-  width: 160px;
-  text-align: center;
-
-  ${ButtonStyles}/* background-color: var(--primary-color);
-  width: 160px;
-  border-radius: 4px;
-  padding: 8px;
-  text-align: center;
-  font-weight: 600;
-  font-size: 1.2rem;
-
-
-  &:hover,
-  &:focus {
-    background-color: var(--primary-color-dark);
-  } */
-`;
-
 export const HeaderWrapper = styled.header`
   display: flex;
   position: relative;
   flex-direction: column;
   align-items: center;
 
-  a {
-    display: inline-block;
+  .back-cta {
+    visibility: ${(props) => (props.notRoot ? 'visible' : 'hidden')};
     text-decoration: none;
-    color: white;
+    position: absolute;
+    left: 0;
+    width: 160px;
+    text-align: center;
 
-    img {
-      margin-bottom: 0px;
-      width: 151px;
-      height: 141px;
-    }
+    ${ButtonStyles}
+  }
+
+  .logo img {
+    margin-bottom: 0px;
+    width: 151px;
+    height: 141px;
   }
 
   h1 {
