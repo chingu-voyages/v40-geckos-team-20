45149afa46a1ae3ae05cb042bcf8e1700a99e7ff
viteshbava <vitesh.bava@gmail.com>
2022-09-06 21:50:48 +1200

    Add media queries to pagination

--- src/components/UI/Pagination/Pagination.styled.js
+++ src/components/UI/Pagination/Pagination.styled.js
@@ -1,16 +1,17 @@
-import styled from "styled-components/macro";
+import styled from 'styled-components/macro';
 
 export const PaginationWrapper = styled.div`
   display: flex;
   justify-content: flex-end;
-  gap: 24px;
+  flex-wrap: wrap;
   margin-top: 1.5rem;
-`
+`;
 
 export const PaginationItem = styled.div`
   width: 24px;
   height: 24px;
+  margin: 12px;
   border-radius: 50%;
-  background-color: ${props => props.isCurrentPage ? "#CA0000" : "#666666"};
+  background-color: ${(props) => (props.isCurrentPage ? '#CA0000' : '#666666')};
   cursor: pointer;
 `;
