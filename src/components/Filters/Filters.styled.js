96247ef26982751af5e90a2d18e9c6dc169505b2
viteshbava <vitesh.bava@gmail.com>
2022-09-06 21:15:07 +1200

    Add media queries for filters

--- src/components/Filters/Filters.styled.js
+++ src/components/Filters/Filters.styled.js
@@ -1,8 +1,32 @@
 import styled from 'styled-components/macro';
+import Media from '../../styles/Media';
+import { DropDownWrapper } from '../UI/DropDown/DropDown.styled';
 
 export const FilterWrapper = styled.div`
   display: flex;
   justify-content: space-around;
   margin: 1rem auto 3rem;
   max-width: 50rem;
+
+  ${Media.upToTablet`
+    justify-content: space-between;
+
+    ${DropDownWrapper} {
+      width: 30%;
+    }
+
+  `}
+
+  ${Media.upToPhone`
+    flex-direction: column;
+
+    ${DropDownWrapper} {
+      width: 100%;
+      margin-bottom: 1.5rem;
+      button {
+        height: 60px;
+      }
+    }
+
+  `}
 `;
